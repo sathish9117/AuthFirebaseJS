2e6163c8867ee07922f899b5792bd7edea9ee59f
Sathish Kumar N <162999084+sathish9117@users.noreply.github.com>
2024-11-02 19:18:42 +0530

    remove unknow data

--- js/home.js
+++ js/home.js
@@ -16,7 +16,7 @@ onAuthStateChanged(auth, (user) => {
   console.log(loggedInUserId);
   if (loggedInUserId) {
     getData();
-    userData.style.display = "block"; // Hides the navBtns, but space remains
+    // userData.style.display = "block"; // Hides the navBtns, but space remains
     console.log("block");
 
     // window.location.href = "admin.html"; // Redirect to index page
@@ -33,7 +33,7 @@ let checkCred = () => {
     window.location.href = "login.html";
     console.log("Not Logged");
   } else {
-    userData.style.display = "block"; // Hides the navBtns, but space remains
+    // userData.style.display = "block"; // Hides the navBtns, but space remains
     console.log("Logged");
   }
 };
@@ -57,8 +57,9 @@ function getData() {
     .then((docSnap) => {
       if (docSnap.exists()) {
         const userData = docSnap.data();
+        document.getElementById("profilePicIndex").src = userData.imageUrl;
         document.getElementById("loggedUserName").innerText = userData.username;
-        document.getElementById("loggedUserEmail").innerText = userData.email;
+        // document.getElementById("loggedUserEmail").innerText = userData.email;
         console.log("userData", userData.email);
       } else {
         console.log("no document found matching id");
@@ -67,13 +68,4 @@ function getData() {
     .catch((error) => {
       console.log("Error getting Document", error);
     });
-}
-
-function NavBtns() {
-  if (userData.style.visibility === "none") {
-    navBtns.style.visibility = "block"; // Makes the navBtns visible
-    userData.style.visibility = "none"; // Makes the navBtns visible
-  } else {
-    navBtns.style.visibility = "none"; // Hides the navBtns, but space remains
-  }
 }
